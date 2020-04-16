0945f0ce4af384b8668f09da45505ef7a66768d1
Ben Beekman <bbeekman@gmail.com>
2020-04-16 00:22:54 -0700

    added kit project page data

--- public/portfolio-data.js
+++ public/portfolio-data.js
@@ -28,7 +28,7 @@ export default {
   'skills': {
     'heading': 'About Ben',
     'techHeading': 'I currently use these technologies:',
-    'techCopy': `As a software developer, I create creative digital solutions using a modern technology stack and a decade of experience with front-end and UI/UX development. My programming language of choice is JavaScript, but I have gained a lot of perspective from working with other languages. Working with Python and PHP has made me a better, more well-rounded developer with the perspective to pick the right tool for the job.</p>`,
+    'techCopy': 'I create creative digital solutions with a modern tech stack and over a decade of experience with front-end development. My programming language of choice is JavaScript, but it\'s not the only one I\'ve worked with. My work with Python and PHP has made me a more well rounded and flexible developer.</p>',
     'tech': [
       {
         'name': 'JavaScript',
@@ -99,7 +99,13 @@ export default {
       },
     ]
   },
-  projects: {
-    'summary': 'In the past 6 months, I worked on small teams that created a progressive web app that helps you improve personal communication, an IoT plant sensor application, and a Tinder-style newsreader. I also have a decade of front-end and UX/UI experience developing WordPress themes and websites, which made me an expert at solving problems for clients.'
+  'projects': {
+    'summary': 'Over the past 6 months, I worked on small teams that created a progressive web app that helps you improve personal communication, an IoT plant sensor application, and a Tinder-style newsreader. I also have a decade of front-end and UX/UI experience developing WordPress themes and websites, which made me an expert at solving problems for clients.'
+    'projects': {
+      'title': 'kit: Keep In Touch',
+      'description': 'A progressive web app for setting, tracking, and meeting communication goals. Users can input contact details, set flexible communication schedules for those contacts, schedule push notification reminders, and view their communication logs. Contact list is sorted and styled based on urgency and proximity to scheduled "hard" and "soft" deadlines.'.
+    },
+    {}
   }
+}
 };
