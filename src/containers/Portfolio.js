2e55fed52a1f708236b220cc8a960568610d39eb
Ben Beekman <bbeekman@gmail.com>
2020-04-20 19:36:34 -0700

    added Nav to Portfolio container, styled links

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -3,13 +3,15 @@ import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact/Contact';
 import Skills from '../components/Skills/Skills';
 import About from '../components/About/About';
+import Nav from '../components/Nav/Nav';
 import styles from '../components/App';
 import data from '../../public/portfolio-data.js';
 export default class Portfolio extends Component {
 
   render() {
     return (
-      <div className={styles.Portfolio}>
+      <div className={styles.Projects}>
+        <Nav />
         <About about={data.about} />
         <Skills skills={data.skills} learning={data.learning} />
         <Projects projects={data.projects} />
