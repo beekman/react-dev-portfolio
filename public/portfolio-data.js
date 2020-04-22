60d4883ff5bbe1475fd1ddb7d985af278cf8ce68
Ben Beekman <bbeekman@gmail.com>
2020-04-22 14:16:29 -0700

    relinked all json images

--- public/portfolio-data.js
+++ public/portfolio-data.js
@@ -3,7 +3,7 @@ export default {
     'title': 'import SoftwareDeveloper from \'benbeekman.dev\';',
     'summary': 'Hi, I\'m Ben. I am a full-stack developer with an extensive background in front-end development, SEO and education. I value clean code that solves human-centric problems and making a meaningful impact for the companies and teams I work for.',
     'pitch': 'I have a degree in multimedia design and over a decade of professional experience with web development and design. Over the past two years, I\'ve fallen in love with programming and drilled deeply into full-stack JavaScript development.',
-    'photo': '../public/assets/ben-beekman-profile-800.jpg'
+    'photo': '../src/assets/ben-beekman-profile-800.jpg'
   },
   'contact': {
     'heading': 'Connect with Ben',
@@ -25,86 +25,86 @@ export default {
     'tech': [
       {
         'name': 'JavaScript',
-        'logo': '../public/assets/tech/thumbs/js-logo-60px.jpeg'
+        'logo': '../src/assets/tech/thumbs/js-logo-60px.jpeg'
       },
       {
         'name': 'React',
-        'logo': '../public/assets/tech/thumbs/react-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/react-logo-60px.png'
       },
       {
         'name': 'Redux',
-        'logo': '../public/assets/tech/thumbs/redux-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/redux-logo-60px.png'
       },
       {
         'name': 'Node',
-        'logo': '../public/assets/tech/thumbs/node-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/node-logo-60px.png'
       },
       {
         'name': 'Express',
-        'logo': '../public/assets/tech/thumbs/express-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/express-logo-60px.png'
       },
       {
         'name': 'MongoDB',
-        'logo': '../public/assets/tech/thumbs/mongodb-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/mongodb-logo-60px.png'
       },
       {
         'name': 'PostgreSQL',
-        'logo': '../public/assets/tech/thumbs/Postgresql-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/Postgresql-logo-60px.png'
       },
       {
         'name': 'Mongoose',
-        'logo': '../public/assets/tech/thumbs/mongoose-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/mongoose-logo-60px.png'
       },
       {
         'name': 'Jest',
-        'logo': '../public/assets/tech/thumbs/jest-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/jest-logo-60px.png'
       },
       {
         'name': 'Git',
-        'logo': '../public/assets/tech/thumbs/github-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/github-logo-60px.png'
       },
       {
         'name': 'Netlify',
-        'logo': '../public/assets/tech/thumbs/netlify-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/netlify-logo-60px.png'
       },
       {
         'name': 'Heroku',
-        'logo': '../public/assets/tech/thumbs/heroku-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/heroku-logo-60px.png'
       },
       {
         'name': 'Python',
-        'logo': '../public/assets/tech/thumbs/python-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/python-logo-60px.png'
       },
       {
         'name': 'PHP',
-        'logo': '../public/assets/tech/thumbs/php-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/php-logo-60px.png'
       },
       {
         'name': 'SASS',
-        'logo': '../public/assets/tech/thumbs/sass-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/sass-logo-60px.png'
       },
       {
         'name': 'HTML',
-        'logo': '../public/assets/tech/thumbs/html-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/html-logo-60px.png'
       },
       {
         'name': 'CSS',
-        'logo': '../public/assets/tech/thumbs/css-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/css-logo-60px.png'
       },
     ],
     'learningCopy': 'I\'m passionate about learning, and I\'m constantly adding new tools to my arsenal. Here are a few technologies that I\'m learning currently.',
     'learning': [
       {
         'name': 'TypeScript',
-        'logo': '../public/assets/tech/thumbs/typescript-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/typescript-logo-60px.png'
       },
       {
         'name': 'GraphQL',
-        'logo': '../public/assets/tech/thumbs/graphql-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/graphql-logo-60px.png'
       },
       {
         'name': 'SocketIO',
-        'logo': '../public/assets/tech/thumbs/socket-logo-60px.png'
+        'logo': '../src/assets/tech/thumbs/socket-logo-60px.png'
       }
     ]
   },
@@ -115,103 +115,103 @@ export default {
       {
         'title': 'kit: Keep In Touch',
         'description': 'A progressive web app for setting, tracking, and meeting communication goals. Users can input contact details, set flexible communication schedules for those contacts, schedule push notification reminders, and view their communication logs. Contact list is sorted and formatted based on urgency and status of scheduled "hard" and "soft" deadlines.',
-        'image': '../public/assets/portfolio/kit.png',
+        'image': '../src/assets/portfolio/kit.png',
         'demo': 'https://friendlier-staging.netlify.app',
         'github': 'https://github.com/band-m/friendlier-fe',
         'tech': [
           {
             'name': 'JavaScript',
-            'logo': '../public/assets/tech/thumbs/js-logo-60px.jpeg'
+            'logo': '../src/assets/tech/thumbs/js-logo-60px.jpeg'
           },
           {
             'name': 'React',
-            'logo': '../public/assets/tech/thumbs/react-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/react-logo-60px.png'
           },
           {
             'name': 'Redux',
-            'logo': '../public/assets/tech/thumbs/redux-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/redux-logo-60px.png'
           },
           {
             'name': 'Node',
-            'logo': '../public/assets/tech/thumbs/node-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/node-logo-60px.png'
           },
           {
             'name': 'Express',
-            'logo': '../public/assets/tech/thumbs/express-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/express-logo-60px.png'
           },
           {
             'name': 'MongoDB',
-            'logo': '../public/assets/tech/thumbs/mongodb-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/mongodb-logo-60px.png'
           },
           {
             'name': 'Jest',
-            'logo': '../public/assets/tech/thumbs/jest-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/jest-logo-60px.png'
           },
           {
             'name': 'Netlify',
-            'logo': '../public/assets/tech/thumbs/netlify-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/netlify-logo-60px.png'
           },
           {
             'name': 'Heroku',
-            'logo': '../public/assets/tech/thumbs/heroku-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/heroku-logo-60px.png'
           },
           {
             'name': 'HTML',
-            'logo': '../public/assets/tech/thumbs/html-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/html-logo-60px.png'
           },
           {
             'name': 'CSS',
-            'logo': '../public/assets/tech/thumbs/css-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/css-logo-60px.png'
           },
         ],
       },
       {
         'title': 'FrontPage',
         'description': 'A Tinder-style newsreader that allows you to swipe right on the news that interests you.',
-        'image': '../public/assets/portfolio/frontpage.png',
+        'image': '../src/assets/portfolio/frontpage.png',
         'demo': 'https://friendlier-staging.netlify.app/login',
         'github': 'https://github.com/band-m/friendlier-fe',
         'tech': [
           {
             'name': 'JavaScript',
-            'logo': '../public/assets/tech/thumbs/js-logo-60px.jpeg'
+            'logo': '../src/assets/tech/thumbs/js-logo-60px.jpeg'
           },
           {
             'name': 'Node',
-            'logo': '../public/assets/tech/thumbs/node-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/node-logo-60px.png'
           },
           {
             'name': 'MongoDB',
-            'logo': '../public/assets/tech/thumbs/mongodb-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/mongodb-logo-60px.png'
           },
           {
             'name': 'HTML',
-            'logo': '../public/assets/tech/thumbs/html-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/html-logo-60px.png'
           },
           {
             'name': 'CSS',
-            'logo': '../public/assets/tech/thumbs/css-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/css-logo-60px.png'
           },
         ],
       },
       {
         'title': 'Qwixx',
         'description': 'A mobile-responsive JavaScript implementation of the popular Qwixx dice game that speeds gameplay and doesn\'t waste scorecards. Developed with face-to-face simultaneous multiplayer play in mind, each player should run a copy on their own device so they can score on the rolls of other players as well as their own.',
-        'image': '../public/assets/portfolio/qwixx.png',
+        'image': '../src/assets/portfolio/qwixx.png',
         'demo': 'https://i-16.github.io/qwixx/',
         'github': 'https://github.com/I-16/qwixx',
         'tech': [
           {
             'name': 'JavaScript',
-            'logo': '../public/assets/tech/thumbs/js-logo-60px.jpeg'
+            'logo': '../src/assets/tech/thumbs/js-logo-60px.jpeg'
           },
           {
             'name': 'HTML',
-            'logo': '../public/assets/tech/thumbs/html-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/html-logo-60px.png'
           },
           {
             'name': 'CSS',
-            'logo': '../public/assets/tech/thumbs/css-logo-60px.png'
+            'logo': '../src/assets/tech/thumbs/css-logo-60px.png'
           },
         ],
       },
