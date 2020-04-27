eba171221b2db6e1a57135ae14303e8e9f67ebbc
Ben Beekman <bbeekman@gmail.com>
2020-04-27 12:46:42 -0700

    navigation menu z-index fix and fancy css glow effects

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import styles from './App.css';
 import Portfolio from '../containers/Portfolio';
 export default function App() {
   return (
-    <main className={styles.App}>
-      <Portfolio />
-    </main>
+    <Portfolio />
   );
 }
