c8efbf411fd2779e762856825e675f0b8e530bf0
Ben Beekman <bbeekman@gmail.com>
2020-11-02 22:58:15 -0800

    added basic config files

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,25 @@ const Dotenv = require('dotenv-webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 
 
-
-
 // eslint-disable-next-line
 module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   entry: './src/index.js',
   output: {
     filename: 'bundle.[hash].js',
